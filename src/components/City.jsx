ad08f46cba348bc21104a981b56cd3d9d821037e
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2023-11-22 13:52:09 +0100

    Implemented useSearchParams and useParams

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -11,6 +11,9 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -21,7 +24,14 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h2>city {id}</h2>;
+  return (
+    <>
+      <h2>city {id}</h2>
+      <parseInt>
+        Position: {lat},{lng}
+      </parseInt>
+    </>
+  );
 }
 
 //   return (
