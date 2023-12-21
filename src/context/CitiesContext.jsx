a4979b0d0602e9ac41379e7c0cadb72d0d7ec5ea
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2023-12-22 00:46:43 +0100

    Added deleteCity, all done using useReducer

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -52,14 +52,36 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       setCities((cities) => [...cities, data]);
     } catch {
-      alert("There was an error loading data...");
+      alert("There was an error adding city...");
     } finally {
       setIsLoading(false);
     }
   }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error deleting city data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
